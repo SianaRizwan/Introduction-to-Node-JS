f78acf74c5659083c8445858f4791c635a3bd25e
Siana Rizwan <sianarizwan@gmail.com>
2021-06-23 19:26:56 +0600

    views added

--- Lab_2_180042105/Task/app.js
+++ Lab_2_180042105/Task/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("Hello");
+    res.sendFile("Hello");
 });
+
+app.use((req,res)=>{
+    
+        res.status(301).send("Page doesn't exist");
+    });
 
 module.exports = app;
