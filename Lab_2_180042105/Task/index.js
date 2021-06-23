789f17b77212ae4985f39ff002f590d0684c3b09
Siana Rizwan <sianarizwan@gmail.com>
2021-06-23 21:01:41 +0600

    Db Connect successfully

--- Lab_2_180042105/Task/index.js
+++ Lab_2_180042105/Task/index.js
@@ -1,8 +1,9 @@
 const app = require('./app');
+const dbConnect = require("./model/dbConnect");
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-
+dbConnect();
 
 app.listen(PORT,()=> {
     console.log (`Server is running at port ${PORT}`);
