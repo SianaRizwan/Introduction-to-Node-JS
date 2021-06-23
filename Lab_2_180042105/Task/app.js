c90454a301f5d84ac5ca6f0400bac3543841776e
Siana Rizwan <sianarizwan@gmail.com>
2021-06-23 21:17:49 +0600

    Saving data in Json format

--- Lab_2_180042105/Task/app.js
+++ Lab_2_180042105/Task/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const userRoutes = require("./routes/userRoutes.routes");
+const morgan = require("morgan");
+const { urlencoded } = require('body-parser');
+
+app.use(userRoutes);
+app.use(express.json());
+app.use(urlencoded({extended:false}));
 
 app.get("/", (req,res)=>{
-    res.sendFile("Hello");
+    res.sendFile("home.html", {root: "./views"});
 });
 
 app.use((req,res)=>{
-    
-        res.status(301).send("Page doesn't exist");
+    res.status(301).send("Page doesn't exist");
     });
 
 module.exports = app;
