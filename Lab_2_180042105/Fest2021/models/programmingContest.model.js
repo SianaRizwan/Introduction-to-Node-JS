1b8400f0796f982765338b7bc76b7a6c2def261a
Siana Rizwan <sianarizwan@gmail.com>
2021-08-04 21:20:54 +0600

    Create programmingContest.model.js

---
+++ Lab_2_180042105/Fest2021/models/programmingContest.model.js
@@ -0,0 +1,102 @@
+const mongoose = require("mongoose");
+const PCSchema = new mongoose.Schema({
+  teamName: {
+    type: String,
+    required: true,
+  },
+  institute: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  paid: {
+    type: Number,
+    required: true,
+  },
+  selected: {
+    type: Boolean,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+
+  coachName: {
+    type: String,
+    required: true,
+  },
+  coachContact: {
+    type: String,
+    required: true,
+  },
+  coachEmail: {
+    type: String,
+    required: true,
+  },
+  coachTshirt: {
+    type: String,
+    required: true,
+  },
+
+  tleadName: {
+    type: String,
+    required: true,
+  },
+  tleadContact: {
+    type: String,
+    required: true,
+  },
+  tleadEmail: {
+    type: String,
+    required: true,
+  },
+  tleadtshirt: {
+    type: String,
+    required: true,
+  },
+
+  tm1Name: {
+    type: String,
+    required: true,
+  },
+  tm1Contact: {
+    type: String,
+    required: true,
+  },
+  tm1Email: {
+    type: String,
+    required: true,
+  },
+  tm1tshirt: {
+    type: String,
+    required: true,
+  },
+
+  tm2Name: {
+    type: String,
+    required: true,
+  },
+  tm2Contact: {
+    type: String,
+    required: true,
+  },
+  tm2Email: {
+    type: String,
+    required: true,
+  },
+  tm2tshirt: {
+    type: String,
+    required: true,
+  },
+})
+
+const ProgContest = mongoose.model("programming-contest", PCSchema);
+module.exports = ProgContest;
